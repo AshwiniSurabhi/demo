82e330fbe4b0145e2bd51990dca02351c8c92199
Aswiniwini <ashwinisurabhi@gmail.com>
2021-04-06 10:37:17 -0500

    Adding Junits

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@InjectMocks
+	DemoApplication demoApplication;
+
 	@Test
-	void contextLoads() {
+	public void sayHelloTest(){
+		String response = demoApplication.sayHello();
+		assertEquals("Hello",response);
+
 	}
+
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
